16e359a4352f1fdc936f29bb413433dfb9799a40
hindujaB <bhinduja08@gmail.com>
2024-04-03 21:27:57 +0530

    Add ballerina implementation

--- ballerina/init.bal
+++ ballerina/init.bal
@@ -14,6 +14,51 @@
 // specific language governing permissions and limitations
 // under the License.
 
-function init() returns error? {
+import ballerina/http;
+import ballerina/lang.runtime;
+import ballerina/log;
 
+function init() returns error? {
+    worker w1 returns error? {
+        check registerInDashboardServer();
+    }
+}
+
+function registerInDashboardServer() returns error? {
+    if dashboard.url == "" {
+        return;
+    }
+    // We need to wait until services are up
+    runtime:sleep(dashboard.waitTimeForServicesInSeconds);
+    http:Client dsClient = check new (dashboard.url,
+        secureSocket = {
+            enable: false
+        }
+    );
+    IntegrationPlaneConnectionRequest connectionRequest = {
+        groupId: dashboard.groupId,
+        nodeId: dashboard.nodeId,
+        interval: dashboard.heartbeatInterval,
+        mgtApiUrl: dashboard.mgtApiUrl
+    };
+    boolean isFailed = true;
+    while (true) {
+        http:Response|http:ClientError resp = dsClient->post("/heartbeat", connectionRequest);
+        if (resp is http:ClientError) {
+            log:printError(string `Connection to dashboard server ${dashboard.url} is failed due to http error ${resp.toString()}. Retrying after ${dashboard.heartbeatInterval} seconds.`);
+            isFailed = true;
+        } else if (resp.statusCode != 200) {
+            string|http:ClientError textPayload = resp.getTextPayload();
+            if textPayload is string {
+                log:printError(string `Connection to dashboard server ${dashboard.url} is failed due to ${textPayload} with error code ${resp.statusCode}. Retrying after ${dashboard.heartbeatInterval} seconds.`);
+            } else {
+                log:printError(string `Connection to dashboard server ${dashboard.url} is failed due to client error ${textPayload.message()}. Retrying after ${dashboard.heartbeatInterval} seconds.`);
+            }
+            isFailed = true;
+        } else if (isFailed) {
+            log:printInfo(string `Connected to dashboard server ${dashboard.url}`);
+            isFailed = false;
+        }
+        runtime:sleep(<decimal>dashboard.heartbeatInterval);
+    }
 }
--- native/src/main/java/io/ballerina/lib/wso2/controlplane/Utils.java
+++ native/src/main/java/io/ballerina/lib/wso2/controlplane/Utils.java
@@ -16,21 +16,144 @@
 
 package io.ballerina.lib.wso2.controlplane;
 
+import io.ballerina.runtime.api.Artifact;
+import io.ballerina.runtime.api.Environment;
 import io.ballerina.runtime.api.Module;
+import io.ballerina.runtime.api.Node;
+import io.ballerina.runtime.api.PredefinedTypes;
+import io.ballerina.runtime.api.TypeTags;
+import io.ballerina.runtime.api.creators.TypeCreator;
 import io.ballerina.runtime.api.creators.ValueCreator;
+import io.ballerina.runtime.api.types.ArrayType;
+import io.ballerina.runtime.api.types.Type;
+import io.ballerina.runtime.api.utils.StringUtils;
+import io.ballerina.runtime.api.utils.TypeUtils;
+import io.ballerina.runtime.api.values.BArray;
+import io.ballerina.runtime.api.values.BError;
+import io.ballerina.runtime.api.values.BListInitialValueEntry;
 import io.ballerina.runtime.api.values.BMap;
+import io.ballerina.runtime.api.values.BObject;
 import io.ballerina.runtime.api.values.BString;
+import io.ballerina.runtime.api.values.BTypedesc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.BALLERINA_HOME;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.BALLERINA_VERSION;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.BAL_HOME;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.BAL_VERSION;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.ID;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.NODE_DATA;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.OS;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.OS_NAME;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.OS_VERSION;
+import static io.ballerina.lib.wso2.controlplane.ControlPlaneConstants.PATH_SEPARATOR;
 
 /**
- * Native function implementations of the wso2 controlplane module.
+ * Native function implementations of the wso2 control plane module.
  *
  * @since 1.0.0
  */
 public class Utils {
 
-    public static BMap<BString, Object> getArtifacts() {
-        Module currentModule = new Module("ballerinai", "remote.management", "1.0.0");
-        BMap<BString, Object> artifactEntries = ValueCreator.createMapValue();
-        return ValueCreator.createReadonlyRecordValue(currentModule, "ArtifactsResponse", artifactEntries);
+    public static Object getBallerinaNode(Environment env) {
+        Module currentModule = env.getCurrentModule();
+        Node node = env.getRepository().getNode();
+        BMap<BString, Object> nodeEntries = ValueCreator.createMapValue();
+        BMap<BString, Object> metadataEntries = ValueCreator.createMapValue();
+        metadataEntries.put(StringUtils.fromString(BALLERINA_VERSION),
+                StringUtils.fromString((String) node.getDetail(BAL_VERSION)));
+        metadataEntries.put(StringUtils.fromString(BALLERINA_HOME),
+                StringUtils.fromString((String) node.getDetail(BAL_HOME)));
+        metadataEntries.put(StringUtils.fromString(OS),
+                StringUtils.fromString((String) node.getDetail(OS_NAME)));
+        metadataEntries.put(StringUtils.fromString(OS_VERSION),
+                StringUtils.fromString((String) node.getDetail(OS_VERSION)));
+        BMap<BString, Object> nodeData = ValueCreator.createReadonlyRecordValue(currentModule,
+                "NodeData", metadataEntries);
+        nodeEntries.put(StringUtils.fromString(ID), StringUtils.fromString(node.nodeId));
+        nodeEntries.put(StringUtils.fromString(NODE_DATA), nodeData);
+        return ValueCreator.createReadonlyRecordValue(currentModule, "Node", nodeEntries);
+    }
+
+    public static Object getArtifacts(Environment env, BString resourceType, Object searchKey, BTypedesc typedesc) {
+        Module currentModule = env.getCurrentModule();
+        Type artifactType = TypeUtils.getImpliedType(typedesc.getDescribingType());
+        List<Artifact> artifacts = env.getRepository().getArtifacts();
+        List<BListInitialValueEntry> artifactEntries = new ArrayList<>();
+        int index = 0;
+        for (Artifact artifact : artifacts) {
+            BObject serviceObj = (BObject) artifact.getDetail("service");
+            Type originalType = serviceObj.getOriginalType();
+            Module module = originalType.getPackage();
+            if (module != null && module.equals(currentModule)) {
+                continue;
+            }
+            BMap<BString, Object> service = ValueCreator.createMapValue();
+            service.put(StringUtils.fromString("name"), StringUtils.fromString(originalType.toString()));
+            service.put(StringUtils.fromString("attachPoint"), getAttachPointString(artifact));
+            service.put(StringUtils.fromString("metadata"), getServiceMetadata(artifact, serviceObj, currentModule));
+            service.put(StringUtils.fromString("annotations"), getServiceAnnotations(artifact, serviceObj,
+                    artifactType));
+            artifactEntries.add(ValueCreator.createListInitialValueEntry(
+                    ValueCreator.createReadonlyRecordValue(currentModule, "Service", service)));
+        }
+        ArrayType arrayType = TypeCreator.createArrayType(artifactType, true);
+        return ValueCreator.createArrayValue(arrayType, artifactEntries.toArray(BListInitialValueEntry[]::new));
+    }
+
+    private static BMap<BString, Object> getServiceMetadata(Artifact artifact, BObject serviceObj, Module module) {
+        BMap<BString, Object> metadata = ValueCreator.createRecordValue(module, "Metadata");
+        List<BObject> listeners = (List<BObject>) artifact.getDetail("listeners");
+        BListInitialValueEntry[] listenerEntries = new BListInitialValueEntry[listeners.size()];
+        for (int i = 0; i < listeners.size(); i++) {
+            BObject listener = listeners.get(i);
+            BMap<BString, Object> listenerRecord = ValueCreator.createMapValue();
+            listenerRecord.put(StringUtils.fromString("type"), StringUtils.fromString(listener.getOriginalType()
+                    .toString()));
+            BMap<BString, Object> properties = getMapAnydataValue();
+            addListenerProperty(listener, "port", properties);
+            listenerRecord.put(StringUtils.fromString("properties"), properties);
+            listenerEntries[i] = ValueCreator.createListInitialValueEntry(ValueCreator.createReadonlyRecordValue(module,
+                    "Listener", listenerRecord));
+        }
+        ArrayType arrayType = TypeCreator.createArrayType(TypeUtils.getType(
+                ValueCreator.createRecordValue(module, "Listener")), true);
+        metadata.put(StringUtils.fromString("listeners"),
+                ValueCreator.createArrayValue(arrayType, listenerEntries));
+        metadata.put(StringUtils.fromString("metadata"), getMapAnydataValue());
+        return metadata;
+    }
+
+    private static BMap<BString, Object> getMapAnydataValue() {
+        return ValueCreator.createMapValue(TypeCreator.createMapType(PredefinedTypes.TYPE_ANYDATA));
+    }
+
+    private static Object getServiceAnnotations(Artifact artifact, BObject serviceObj, Type artifactType) {
+        return getMapAnydataValue();
+    }
+
+
+    private static void addListenerProperty(BObject listener, String fieldName, BMap<BString, Object> properties) {
+        try {
+            Object value = listener.get(StringUtils.fromString(fieldName));
+            properties.put(StringUtils.fromString(fieldName), value);
+        } catch (BError e) {
+            // this means no such field in the object
+        }
+    }
+
+    private static Object getAttachPointString(Artifact artifact) {
+        Object attachPoint = artifact.getDetail("attachPoint");
+        if (TypeUtils.getType(attachPoint).getTag() == TypeTags.ARRAY_TAG) {
+            BArray array = (BArray) attachPoint;
+            StringBuilder attachPointStr = new StringBuilder();
+            for (int i = 0; i < array.size(); i++) {
+                attachPointStr.append(PATH_SEPARATOR).append(array.getBString(i).getValue());
+            }
+            return StringUtils.fromString(attachPointStr.toString());
+        }
+        return attachPoint;
     }
 }
