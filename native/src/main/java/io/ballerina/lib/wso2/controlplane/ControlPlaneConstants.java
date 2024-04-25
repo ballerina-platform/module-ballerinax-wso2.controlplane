e521093cfde720311880cba7a1870279674e73f2
hindujaB <bhinduja08@gmail.com>
2024-04-25 21:53:13 +0530

    Refactor added APIs

--- native/src/main/java/io/ballerina/lib/wso2/controlplane/ControlPlaneConstants.java
+++ native/src/main/java/io/ballerina/lib/wso2/controlplane/ControlPlaneConstants.java
@@ -7,9 +7,7 @@ public class ControlPlaneConstants {
     public static final String BAL_HOME = "balHome";
     public static final String BAL_VERSION = "balVersion";
     public static final String OS_NAME = "osName";
-    public static final String PATH_SEPARATOR = "/";
     public static final String SERVICES_RESOURCE = "services";
-
     public static final String SINGLE_SLASH = "/";
     public static final String PATH_PARAM_IDENTIFIER = "^";
     public static final String OPEN_CURL_IDENTIFIER = "{";
@@ -18,4 +16,30 @@ public class ControlPlaneConstants {
     public static final String STAR_IDENTIFIER = "*";
     public static final String DOT_IDENTIFIER = ".";
     public static final String REGEX = "(?<!(http:|https:))//";
+    public static final String SERVICE = "service";
+    public static final String LISTENERS = "listeners";
+    public static final String ATTACH_POINT = "attachPoint";
+    public static final String PACKAGE = "package";
+    public static final String NAME = "name";
+    public static final String PROTOCOL = "protocol";
+    public static final String PORT = "port";
+    public static final String INFERRED_CONFIG = "inferredConfig";
+    public static final String SECURE_SOCKET = "secureSocket";
+    public static final String HTTP = "HTTP";
+    public static final String HTTPS = "HTTPS";
+    public static final String URL = "url";
+    public static final String METHODS = "methods";
+    public static final String BASE_PATH = "basePath";
+    public static final String RESOURCES = "resources";
+    public static final String HTTP_VERSION = "httpVersion";
+    public static final String HOST = "host";
+    public static final String TIMEOUT = "timeout";
+    public static final String REQUEST_LIMITS = "requestLimits";
+    public static final String LISTENER = "Listener";
+    public static final String RESOURCE = "Resource";
+    public static final String SERVICE_RECORD = "Service";
+    public static final String SERVICE_DETAIL = "ServiceDetail";
+    public static final String LISTENER_DETAIL = "ListenerDetail";
+    public static final String REQUEST_LIMIT = "RequestLimit";
+    public static final String NODE = "Node";
 }
